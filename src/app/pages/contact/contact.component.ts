32e5588a5a4935b76cdb883ef4b3a66bf1642151
AmiraEltaher <amiraeltaher2020@gmail.com>
2026-09-02 09:44:26 +0300

    implement contact component using reactive form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name   : new FormControl ('', Validators.required),
+    email  : new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
+  isSubmitted = false;
+
+  ngOnInit(){
+     this.contactForm.valueChanges.subscribe(() => {
+  this.isSubmitted = false;
+});
+  }
+   onSubmit(){
+    if (this.contactForm.valid){
+            console.log(this.contactForm.value);
+            this.isSubmitted =true;
+            this.contactForm.reset({emitEvent:false});
+
+    }
+    else{
+      this.contactForm.markAllAsTouched();
+    }
+
+   }
 
 }
